6282888df51116beeeaf89af221e02d2343a7e37
unknown <you@example.com>
2022-11-05 10:39:24 +0530

    harsh bhaiya

---
+++ harshbhaiya1/script.js
@@ -0,0 +1,17 @@
+gsap.to(".image",{
+    ease: Expo.easeInout,
+    width:"100%",
+    stagger:2,
+})
+gsap.to(".text h1",{
+    ease: Expo.easeInout,
+    top:0,
+    stagger:2,
+})
+gsap.to(".text h1",{
+    delay:1.8,
+    ease: Expo.easeInout,
+    top:"-100%",
+    stagger:2,
+    
+})
